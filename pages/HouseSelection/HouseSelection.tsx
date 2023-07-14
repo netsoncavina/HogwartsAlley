211ca6d6bcdf18490bd2e2fcb43779bb7521281a
Netson Cavina <netsoncavina@gmail.com>
2023-07-14 20:52:52 -0300

    Código inicial HouseSelection

---
+++ pages/HouseSelection/HouseSelection.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HouseSelection = () => {
+  return (
+    <View>
+      <Text>HouseSelection</Text>
+    </View>
+  );
+};
+
+export default HouseSelection;
+
+const styles = StyleSheet.create({});
