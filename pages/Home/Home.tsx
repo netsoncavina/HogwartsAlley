506dfa6680b6b2901d5644c480dc940abf7eb0bd
Netson Cavina <netsoncavina@gmail.com>
2023-07-14 20:45:13 -0300

    Desenvolvimento pagina Home

---
+++ pages/Home/Home.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import { useFonts } from "expo-font";
+
+export default function Home() {
+  const [loaded] = useFonts({
+    HarryPotter: require("../../assets/fonts/HarryPotter.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/logos/background.jpg")}
+        style={{ height: "100%", width: "100%", position: "absolute" }}
+        imageStyle={{ opacity: 0.2, backgroundColor: "black" }}
+      />
+      <View style={styles.containerLogo}>
+        <Animatable.Image
+          delay={500}
+          animation="flipInY"
+          duration={2000}
+          source={require("../../assets/logos/hogwarts-logo.png")}
+          style={{ height: 400 }}
+          resizeMode="contain"
+        />
+        <Animatable.Text delay={1500} animation="fadeInUp" style={styles.text}>
+          Welcome to
+        </Animatable.Text>
+        <Animatable.Text delay={1650} animation="fadeInUp" style={styles.text}>
+          Hogwarts
+        </Animatable.Text>
+        <Animatable.Text delay={1800} animation="fadeInUp" style={styles.text}>
+          Alley
+        </Animatable.Text>
+      </View>
+      <Animatable.View
+        delay={2000}
+        animation="fadeInUp"
+        style={styles.buttonContainer}
+      >
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Enter</Text>
+        </TouchableOpacity>
+      </Animatable.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerLogo: {
+    // flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 110,
+  },
+
+  text: {
+    fontFamily: "HarryPotter",
+    fontSize: 70,
+    color: "#fdfdfd",
+    textAlign: "center",
+  },
+  buttonContainer: {
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    // paddingTop: 100,
+  },
+
+  button: {
+    backgroundColor: "#1b1b1b",
+    width: 300,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#fdfdfd",
+    fontWeight: "bold",
+  },
+});
