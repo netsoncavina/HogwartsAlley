b628741adb58ae54866141d584f56a38a5ee00e7
Netson Cavina <netsoncavina@gmail.com>
2023-07-17 19:54:16 -0300

    Adicionada rota para StudentPage

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,11 +3,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../pages/Home/Home";
 import HouseSelection from "../pages/HouseSelection/HouseSelection";
 import HouseStudents from "../pages/HouseStudents/HouseStudents";
+import StudentPage from "../pages/StudentPage/StudentPage";
 
 export type RootStackParamList = {
   Home: undefined;
   HouseSelection: undefined;
   HouseStudents: { houseName: string };
+  StudentPage: { id: string };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -28,6 +30,11 @@ const Routes = () => {
       <Stack.Screen
         name="HouseStudents"
         component={HouseStudents as any}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="StudentPage"
+        component={StudentPage as any}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
