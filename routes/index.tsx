babf0bdb543c6bdeb5f09154dac1797e0ea337a9
Netson Cavina <netsoncavina@gmail.com>
2023-07-16 22:55:52 -0300

    Adição da rota HouseStudents

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../pages/Home/Home";
 import HouseSelection from "../pages/HouseSelection/HouseSelection";
+import HouseStudents from "../pages/HouseStudents/HouseStudents";
 
 export type RootStackParamList = {
   Home: undefined;
   HouseSelection: undefined;
+  HouseStudents: { houseName: string };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,6 +23,11 @@ const Routes = () => {
       <Stack.Screen
         name="HouseSelection"
         component={HouseSelection}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="HouseStudents"
+        component={HouseStudents as any}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
